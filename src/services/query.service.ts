adb08efd239d681a67ab97159ecd2ba17e96600f
amiacs <amiacs@gmail.com>
2022-08-23 19:12:24 +0200

    Adding search functionality for emails, domains, twitter, etc.

--- src/services/query.service.ts
+++ src/services/query.service.ts
@@ -1,4 +1,6 @@
 import { validate } from 'multicoin-address-validator';
+import validator from 'validator';
+import extractDomain from 'extract-domain';
 import FirestoreService from './firestore.service';
 
 class QueryService {
@@ -6,19 +8,78 @@ class QueryService {
 
   public async queryString(stringQuery: string): Promise<any> {
     let queryCollection = 'unknown';
+    let analysisResult = 'caution';
+    let analysisSafetyScore = 50;
+    let analysisMethod = 'ML';
 
     if (validate(stringQuery, 'btc')) {
       queryCollection = 'btc';
-    }
-    if (validate(stringQuery, 'eth')) {
+    } else if (validate(stringQuery, 'eth')) {
       queryCollection = 'eth';
+    } else if (validator.isEmail(stringQuery)) {
+      queryCollection = 'email';
+      stringQuery = validator.normalizeEmail(stringQuery, {
+        all_lowercase: true,
+        gmail_lowercase: true,
+        gmail_remove_dots: true,
+        gmail_remove_subaddress: true,
+        gmail_convert_googlemaildotcom: true,
+        outlookdotcom_lowercase: true,
+        outlookdotcom_remove_subaddress: true,
+        yahoo_lowercase: true,
+        yahoo_remove_subaddress: true,
+        icloud_lowercase: true,
+        icloud_remove_subaddress: true,
+      });
+      console.log('Email: {0}', stringQuery);
+    } else if (validator.isURL(stringQuery)) {
+      queryCollection = 'domains';
+      stringQuery = extractDomain(stringQuery);
+      console.log('Domain: {0}', stringQuery);
+    } else if (stringQuery.startsWith('@')) {
+      queryCollection = 'twitter';
+      console.log('Twitter: {0}', stringQuery);
     }
 
+    console.log('Coll: {0}, Query: {1}', queryCollection, stringQuery);
+    // Search blacklists first
     let queryValue = await this.firestoreService.getDoc(queryCollection, stringQuery);
     if (queryValue.data()) {
-      return { ...queryValue.data(), collection: queryCollection, id: queryValue.id };
+      analysisResult = 'fraud';
+      analysisSafetyScore = 0;
+      analysisMethod = 'blacklist';
+    } else {
+      // then search whitelists
+      console.log('Firebase alternate colelction: {0}', 'wl-' + queryCollection);
+      queryValue = await this.firestoreService.getDoc('wl-' + queryCollection, stringQuery);
+      if (queryValue.data()) {
+        analysisResult = 'safe';
+        analysisSafetyScore = 100;
+        analysisMethod = 'whitelist';
+      }
     }
-    return null;
+
+    const dhResult = queryValue.data();
+    if (queryValue.data()) {
+      if (!dhResult['source']) {
+        dhResult['source'] = 'dirtyhash';
+      }
+      return {
+        result: analysisResult,
+        id: queryValue.id,
+        collection: queryCollection,
+        safetyScore: analysisSafetyScore,
+        method: analysisMethod,
+        ...dhResult,
+      };
+    }
+    return {
+      result: analysisResult,
+      id: queryValue.id,
+      collection: queryCollection,
+      reputationScore: analysisSafetyScore,
+      method: analysisMethod,
+    };
   }
 }
 
