71dab9825ab4f6184f340924079ebd46a34c4cae
amiacs <amiacs@gmail.com>
2022-08-25 14:30:33 +0200

    remove dead code

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import App from '@/app';
-import { AuthController } from '@controllers/auth.controller';
-import { UsersController } from '@controllers/users.controller';
+// import { AuthController } from '@controllers/auth.controller';
+// import { UsersController } from '@controllers/users.controller';
 import { QueryController } from './controllers/query.controller';
 import validateEnv from '@utils/validateEnv';
 import { ReportController } from './controllers/report.controller';
@@ -8,5 +8,5 @@ import { HealthController } from './controllers/health.controller';
 
 validateEnv();
 
-const app = new App([AuthController, UsersController, QueryController, ReportController, HealthController]);
+const app = new App([/*AuthController, UsersController,*/ QueryController, ReportController, HealthController]);
 app.listen();
